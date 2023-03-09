cbb4509fce5915124331940963a2bd5aac9ad067
Yurii Bondar <yuriybondar84@gmail.com>
2023-03-09 23:12:06 +0200

    Chapter 1

--- exercise_files/Ch01/01_03b/src/app.js
+++ exercise_files/Ch01/01_03b/src/app.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {number} contactId 
+ * @returns 
+ */
 async function getContact(contactId) {
   const resp = await $.ajax({
     url: `/contacts/${contactId}`,
@@ -13,9 +18,9 @@ async function getContact(contactId) {
 
 getContact(1).then((contact) => {
   contact.id = "1234"
-  contact.birthDate = "12/12/1990";
+  contact.birthDate = new Date("12/12/1990");
 });
 
-getContact("2").then((contact) => {
+getContact(2).then((contact) => {
   console.log("Contact: ", JSON.stringify(contact));
 });
---
+++ exercise_files/Ch01/01_Challenge/dist/app.js
@@ -0,0 +1,2 @@
+var formattedDate = formatDate(new Date());
+console.log("Today's date is ".concat(formattedDate));
